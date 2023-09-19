7ba2d32d67af200f1c6d2ee42d62f9803b0f9929
adozz78 <adrien.ozanne@gmail.com>
2023-09-19 12:58:57 +0200

    edit hello world function

--- main.py
+++ main.py
@@ -7,4 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-7E4M7T0YEB"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'G-7E4M7T0YEB');
+</script>
+ """
+ return prefix_google + "Hello World"
